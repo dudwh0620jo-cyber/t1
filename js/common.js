ef234ee59ef9e7c7702f9f337dd7c75ca66f87b8
dudwh0620jo-cyber <dudwh0620jo@gmail.com>
2026-04-02 09:26:46 +0900

    집 수정

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    const hamMenu = document.querySelector('.hamMenu');
 
+    if (hamMenu) {
+        hamMenu.addEventListener('click', () => {
+            header.classList.toggle('on');
+        });
+    }
 });//dom end
